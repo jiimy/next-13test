2a3a1521aaa3fa80748897c2e9e6bd6b6251e821
rudwnok <rudwnok123@naver.com>
2023-10-14 01:24:58 +0900

    수정

--- src/pages/api/navermap/route.ts
+++ src/pages/api/navermap/route.ts
@@ -4,24 +4,27 @@
 import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 import { type NextRequest, NextResponse } from "next/server";
-import { useSearchParams } from 'next/navigation';
+import { useSearchParams } from "next/navigation";
 
-export async function GET(req: NextRequest, res: NextApiResponse) {
-  const searchParams = req.nextUrl.searchParams
-  console.log("dd", searchParams);
-  const query = searchParams.get('query')
-  const response = await axios
-    .get("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode", {
-      params: {
-        query: query,
-      },
-      headers: {
-        "X-NCP-APIGW-API-KEY-ID": "ugm16gkcw2",
-        "X-NCP-APIGW-API-KEY": "mDTqyhP8j8bw0IPW8dOaPDG0Wm2VRiSzyqvMotRw",
-      },
-    })
+export async function GET(req: NextApiRequest, res: NextApiResponse) {
+  const { query } = req;
+  // console.log(name, keyword, method);
+
+  // const searchParams = req.nextUrl.searchParams;
+  // console.log("dd", searchParams);
+  // const query = searchParams.get("query");
+  const response = await axios.get("https://naveropenapi.apigw.ntruss.com/map-geocode/v2/geocode", {
+    params: {
+      query: query,
+    },
+    headers: {
+      "X-NCP-APIGW-API-KEY-ID": "ugm16gkcw2",
+      "X-NCP-APIGW-API-KEY": "mDTqyhP8j8bw0IPW8dOaPDG0Wm2VRiSzyqvMotRw",
+    },
+  });
   const data = await response.data;
-  return NextResponse.json({ data })
+  // return NextResponse.json({ data });
+  return res.status(200).json({ data });
 }
 
 // export async function GET(request: NextRequest) {
